7ac34a1e30c897b239681b6ef57f2d7305d70f06
Artem Zaiats <artem.zaiats.211@gmail.com>
2023-06-13 17:18:59 +0300

    Create EditImage Screen Add gallery image picker

---
+++ app/src/main/java/com/example/photoeditor/EditActivity.kt
@@ -0,0 +1,48 @@
+package com.example.photoeditor
+
+import android.net.Uri
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import coil.compose.rememberAsyncImagePainter
+import com.example.photoeditor.ui.theme.PhotoEditorTheme
+
+class EditActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            PhotoEditorTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    val uriString = intent.extras?.getString("imageUri")
+                    val uri = Uri.parse(uriString)
+
+                    EditImage(uri = uri)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun EditImage(uri: Uri?) {
+    val painter = rememberAsyncImagePainter(uri)
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(painter = painter, contentDescription = null)
+    }
+}
---
+++ app/src/main/java/com/example/photoeditor/MainActivity.kt
@@ -0,0 +1,102 @@
+package com.example.photoeditor
+
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.photoeditor.ui.theme.PhotoEditorTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            PhotoEditorTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    SelectImage()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun SelectImage() {
+    val context = LocalContext.current
+    val imageUri = remember { mutableStateOf<Uri?>(null) }
+    val imagePicker = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.GetContent(),
+        onResult = {uri ->
+            imageUri.value = uri
+            launchHandlingActivity(context, uri)
+        }
+    )
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceEvenly
+    ) {
+        Text(text = "Import an image", fontSize = 25.sp, fontWeight = FontWeight.Bold)
+
+        Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceEvenly) {
+            Button(
+                modifier = Modifier
+                    .padding(8.dp)
+                    .width(120.dp)
+                    .height(120.dp)
+                    .clip(CircleShape),
+                onClick = { imagePicker.launch("image/*") }
+            ) {
+                Text(text = "Picker")
+            }
+
+            Button(
+                modifier = Modifier
+                    .padding(8.dp)
+                    .width(120.dp)
+                    .height(120.dp)
+                    .clip(CircleShape),
+                onClick = { /*TODO*/ }
+            ) {
+                Text(text = "Camera")
+            }
+        }
+    }
+}
+
+fun launchHandlingActivity(context: Context, uri: Uri?) {
+    val intent = Intent(context, EditActivity::class.java)
+    intent.putExtra("imageUri", uri.toString())
+    context.startActivity(intent)
+}
